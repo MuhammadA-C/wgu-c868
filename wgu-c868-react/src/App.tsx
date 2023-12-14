5c2f892d24ae6e3cb714c836bcbb9d689757d01d
MuhammadA-C <phantomganhdi@gmail.com>
2023-12-14 01:32:50 -0800

    Completed Page Routing
    Completed setting up the first part of the page routing with the URL.
    
    Next, need to setup the links to the different pages in the top nav bar.

--- wgu-c868-react/src/App.tsx
+++ wgu-c868-react/src/App.tsx
@@ -7,9 +7,12 @@ import { OrdersPage as CustomerOrdersPage } from "./pages/customer/orders/Orders
 import { OrdersPage as OwnerOrdersPage } from "./pages/owner/orders/Orders";
 import CheckOutPage from "./pages/customer/check-out/CheckOut";
 import InventoryPage from "./pages/owner/inventory/Inventory";
+import ReportsPage from "./pages/owner/reports/Reports";
 
 const router = createBrowserRouter([
+  //Sets up all the page routing for the top nav tabs for the application
   {
+    //Below are the pages that the customer will have access to
     path: "/",
     element: <RootLayout />,
     children: [
@@ -20,10 +23,12 @@ const router = createBrowserRouter([
     ],
   },
   {
+    //Below are the pages that the resturant owner will have access to
     path: "/owner",
     children: [
       { path: "/owner", element: <OwnerOrdersPage /> },
       { path: "/owner/menu", element: <OwnerMenuPage /> },
+      { path: "/owner/reports", element: <ReportsPage /> },
       { path: "/owner/inventory", element: <InventoryPage /> },
     ],
   },
