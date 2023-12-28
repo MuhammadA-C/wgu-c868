143d5fa4ea17e826f3d2db34ca1cc30c321e9eed
MuhammadA-C <phantomganhdi@gmail.com>
2023-12-27 16:05:59 -0800

    Customer Menu Page Decrease Count
    Added the code for the customer menu page to decrease the count for the menu items added to the cart.

--- front-end/src/components/table-2/Table.tsx
+++ front-end/src/components/table-2/Table.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 function setCountOnPageLoad(itemID: string) {
   const cart = JSON.parse(
-    localStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
+    sessionStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
   );
 
   if (Object.keys(cart).length == 0) {
@@ -24,25 +24,74 @@ function setCountOnPageLoad(itemID: string) {
 
 function increaseCount(itemID: string, setCount: Function) {
   const cart = JSON.parse(
-    localStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
+    sessionStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
   );
 
   if (Object.keys(cart).length == 0) {
     // Creates the object and property if it does not exist
     cart[itemID] = 1;
-    localStorage.setItem(LocalStorageKeys.customer_cart, JSON.stringify(cart));
+    sessionStorage.setItem(
+      LocalStorageKeys.customer_cart,
+      JSON.stringify(cart)
+    );
   } else if (itemID in cart == false) {
     // Creates the property when it does not exist
     cart[itemID] = 1;
-    localStorage.setItem(LocalStorageKeys.customer_cart, JSON.stringify(cart));
+    sessionStorage.setItem(
+      LocalStorageKeys.customer_cart,
+      JSON.stringify(cart)
+    );
   } else {
     // Increases the count when the property exists
     cart[itemID] = cart[itemID] + 1;
-    localStorage.setItem(LocalStorageKeys.customer_cart, JSON.stringify(cart));
+    sessionStorage.setItem(
+      LocalStorageKeys.customer_cart,
+      JSON.stringify(cart)
+    );
   }
 
   const cart2 = JSON.parse(
-    localStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
+    sessionStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
+  );
+
+  setCount(cart2[itemID]);
+}
+
+function decreaseCount(itemID: string, setCount: Function) {
+  const cart = JSON.parse(
+    sessionStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
+  );
+
+  // Checks if the cart object exists
+  if (Object.keys(cart).length == 0) {
+    return;
+  }
+
+  // Checks if the property exists in the object
+  if (itemID in cart == false) {
+    return;
+  }
+
+  // Checks if decreasing the count will result in it being less than or equal to 0
+  if (cart[itemID] - 1 <= 0) {
+    delete cart[itemID]; // deletes the property from the object
+
+    sessionStorage.setItem(
+      LocalStorageKeys.customer_cart,
+      JSON.stringify(cart)
+    ); // Updates the session storage by removing the property
+
+    setCount(0);
+    return;
+  }
+
+  // Decreases the count by -1 if none of the above checks are true
+  cart[itemID] = cart[itemID] - 1;
+
+  sessionStorage.setItem(LocalStorageKeys.customer_cart, JSON.stringify(cart));
+
+  const cart2 = JSON.parse(
+    sessionStorage.getItem(LocalStorageKeys.customer_cart) || "{}"
   );
 
   setCount(cart2[itemID]);
@@ -69,7 +118,9 @@ function TableItem({
           +
         </button>
         <h2>{count}</h2>
-        <button>-</button>
+        <button onClick={() => decreaseCount(String(itemID), setCount)}>
+          -
+        </button>
       </div>
     </div>
   );
