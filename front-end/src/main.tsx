8d3de9b22d311221135b2d76d0366f0fb391543a
MuhammadA-C <phantomganhdi@gmail.com>
2023-12-30 21:44:07 -0800

    commit

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+/* 
+    Muhammad Chambers
+    Western Governors University
+    C868 Software Development Capstone
+    12/30/2023
+*/
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+/*
+    IMPORTANT TO NOTE: This project was developed for a M1 MacBook Pro 16 inch for Google Chrome
+*/
